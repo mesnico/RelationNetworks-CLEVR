646040bac6f12bd6d3bb375429a7d3557006319f
Fabio Carrara <fabio.carrara@isti.cnr.it>
2017-12-04 17:33:00 +0100

    (partially) fixed LSTM branch

--- model.py
+++ model.py
@@ -73,19 +73,18 @@ class RN(BasicModel):
         super(RN, self).__init__(args, 'RN')
 
         self.args = args
-
         self.qembed_size = 32
-        self.lstm_layers = 128
-
-        self.wembedding = nn.Embedding(args.qdict_size+1, 32, padding_idx=0)  #word embeddings have size 32. Indexes 0 output 0 vectors (variable len questions)
-        self.lstm = nn.LSTM(32, self.qembed_size, self.lstm_layers)  # Input dim is 32, output dim is the question embedding. 128 recurrent layers
+        self.hidden_size = 128
+        
+        self.wembedding = nn.Embedding(args.qdict_size+1, self.qembed_size, padding_idx=0)  #word embeddings have size 32. Indexes 0 output 0 vectors (variable len questions)
+        self.lstm = nn.LSTM(self.qembed_size, self.hidden_size)  # Input dim is 32, output dim is the question embedding
         # initialize the hidden and cell states of the lstm.
         
         self.conv = ConvInputModel()
         self.conv_out_size = 8	#calculated from actual convolutional layer parameters
         
         ##(number of filters per object+coordinate of object)*2+question vector
-        self.g_fc1 = nn.Linear((24+2)*2+self.qembed_size, 512)
+        self.g_fc1 = nn.Linear((24+2)*2+self.hidden_size, 512)
 
         self.g_fc2 = nn.Linear(512, 512)
         self.g_fc3 = nn.Linear(512, 512)
@@ -122,8 +121,8 @@ class RN(BasicModel):
         self.optimizer = optim.Adam(self.parameters(), lr=args.lr)
 
     def init_lstm_hidden(self):
-        hidden_state = torch.randn(self.lstm_layers, self.args.batch_size, self.qembed_size)
-        cell_state = torch.randn(self.lstm_layers, self.args.batch_size, self.qembed_size)
+        hidden_state = torch.randn(1, self.args.batch_size, self.hidden_size)
+        cell_state = torch.randn(1, self.args.batch_size, self.hidden_size)
         if self.args.cuda:
             hidden_state = hidden_state.cuda()
             cell_state = cell_state.cuda()
@@ -144,8 +143,8 @@ class RN(BasicModel):
         #wembed = wembed.view(len(qst_idxs[0]), self.args.batch_size , -1)
         #pdb.set_trace()
 
-        lstm_out, self.hidden = self.lstm(wembed, self.hidden)
-        qst = lstm_out[-1]  # last layer of the lstm. qst = (64 x 32)
+        _, self.hidden = self.lstm(wembed, self.hidden)
+        qst = self.hidden[1] # last layer of the lstm. qst = (64 x 128)
 
         x = self.conv(img) ## x = (64 x 24 x 8 x 8)
         
@@ -157,25 +156,26 @@ class RN(BasicModel):
         x_flat = x.view(mb,n_channels,d*d).permute(0,2,1)
         
         # add coordinates
+        # x_flat = (64 x 64 x 24+2)
         x_flat = torch.cat([x_flat, self.coord_tensor],2)
         
         # add question everywhere
-        qst = torch.unsqueeze(qst, 1)   #(64 x 1 x 32)
-        qst = qst.repeat(1,self.conv_out_size**2,1) #(64 x 64 x 32)
-        qst = torch.unsqueeze(qst, 2)   #(64 x 64 x 1 x 32)
+        qst = torch.unsqueeze(qst, 1)   #(64 x 1 x 128)
+        qst = qst.repeat(1,self.conv_out_size**2,1) #(64 x 64 x 128)
+        qst = torch.unsqueeze(qst, 2)   #(64 x 64 x 1 x 128)
         
         # cast all pairs against each other
         x_i = torch.unsqueeze(x_flat,1) # (64 x 1 x 64 x 26)
         x_i = x_i.repeat(1,self.conv_out_size**2,1,1) # (64 x 64 x 64 x 26)
         x_j = torch.unsqueeze(x_flat,2) # (64 x 64 x 1 x 26)
-        x_j = torch.cat([x_j,qst],3)
-        x_j = x_j.repeat(1,1,self.conv_out_size**2,1) # (64 x 64 x 64 x 26+32)
+        x_j = torch.cat([x_j, qst],3)
+        x_j = x_j.repeat(1,1,self.conv_out_size**2,1) # (64 x 64 x 64 x 26+128)
         
         # concatenate all together
-        x_full = torch.cat([x_i,x_j],3) # (64 x 64 x 64 x 2*26+32)
+        x_full = torch.cat([x_i,x_j],3) # (64 x 64 x 64 x 2*26+128)
         
         # reshape for passing through network
-        x_ = x_full.view(mb*d*d*d*d,2*26+self.qembed_size)
+        x_ = x_full.view(mb*d*d*d*d,2*26+self.hidden_size)
         x_ = self.g_fc1(x_)
         x_ = F.relu(x_)
         x_ = self.g_fc2(x_)
