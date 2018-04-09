b2909e8623f40d4e5d46549673ae1f4024d253ec
mesnico <nicola.messina93@gmail.com>
2018-04-09 17:09:43 +0200

    Added state-description dataset support

---
+++ config.py
@@ -0,0 +1,66 @@
+hyperparams = {
+    "original-fp": #original model from pixels
+        {
+            "g_fc1": 256,
+            "g_fc2": 256,
+            "g_fc3": 256,
+            "g_fc4": 256,
+            
+            "f_fc1": 256,
+            "f_fc2": 256,
+            
+            "dropout": 0.5,
+            "lstm_hidden": 128,
+            "lstm_word_emb": 32,
+            "rl_in_size": (24 + 2)*2
+        },
+    "original-sd": #original model state description
+        {
+            "g_fc1": 512,
+            "g_fc2": 512,
+            "g_fc3": 512,
+            "g_fc4": 512,
+            
+            "f_fc1": 512,
+            "f_fc2": 1024,
+            
+            "dropout": 0.02,
+            "lstm_hidden": 256,
+            "lstm_word_emb": 32,
+            "rl_in_size": 8*2
+        },
+    "ir-fp": #IR model from pixels
+        {
+            "g_fc1": 256,
+            "g_fc2": 256,
+            "g_fc3": 256,
+            "g_fc4": 256,
+            
+            "f_fc1": 256,
+            "f_fc2": 256,
+            
+            "h_fc1": 256,
+            
+            "dropout": 0.5,
+            "lstm_hidden": 128,
+            "lstm_word_emb": 32,
+            "rl_in_size": (24 + 2)*2
+        },
+    "ir-sd": #IR model state description
+        {
+            "g_fc1": 512,
+            "g_fc2": 512,
+            "g_fc3": 512,
+            "g_fc4": 512,
+            
+            "f_fc1": 512,
+            "f_fc2": 1024,
+            
+            "h_fc1": 512,
+            
+            "dropout": 0.02,
+            "lstm_hidden": 256,
+            "lstm_word_emb": 32,
+            "rl_in_size": 7*2
+        },
+}
